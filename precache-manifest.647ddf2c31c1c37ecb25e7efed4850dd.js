2a9337228b091f5346fbeb65fcf101880a65f8bb
Jonas M <jonasmorel8@gmail.com>
2018-10-27 15:55:15 -0400

    Updates

---
+++ precache-manifest.647ddf2c31c1c37ecb25e7efed4850dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e47f624ffeeec66be0c833f4bff5973",
+    "url": "/github.io/clickygame/static/media/background.4e47f624.jpg"
+  },
+  {
+    "revision": "6fa6673cb51c9d86a338",
+    "url": "/github.io/clickygame/static/js/runtime~main.6fa6673c.js"
+  },
+  {
+    "revision": "d9eb3a4a2c8c70d74fae",
+    "url": "/github.io/clickygame/static/js/main.d9eb3a4a.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/github.io/clickygame/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "d9eb3a4a2c8c70d74fae",
+    "url": "/github.io/clickygame/static/css/main.8fe912c9.chunk.css"
+  },
+  {
+    "revision": "26d242504b6582786fcf3e2b33c7f48f",
+    "url": "/github.io/clickygame/index.html"
+  }
+];
